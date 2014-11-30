f5826bd7172e6ab197799aed377f1756a02b3adc
jvba <jvba@cin.ufpe.br>
2014-11-30 18:35:12 +0000

    [JVBA] - Sair do Sistema

--- OnLance/src/com/app/onlance/ConfigGeralActivity.java
+++ OnLance/src/com/app/onlance/ConfigGeralActivity.java
@@ -1,7 +1,11 @@
 package com.app.onlance;
 
+import com.facebook.Session;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConfigGeralActivity extends Activity {
 
@@ -11,4 +15,15 @@ public class ConfigGeralActivity extends Activity {
 		setContentView(R.layout.config_geral);
 	}
 
+	public void onClickSair(View view)
+	{
+		if(Session.getActiveSession()!=null)
+		{
+			Session.getActiveSession().closeAndClearTokenInformation();
+			Session.setActiveSession(null);
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+		}
+		
+	}
 }
