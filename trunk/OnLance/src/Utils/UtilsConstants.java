05869ab3427581b0bb2cac9abf0fe4c4d778e023
kcpo@cin.ufpe.br <kcpo@cin.ufpe.br@57d5b693-2e3a-9804-4996-bd4734186efa>
2014-11-30 18:59:18 +0000

    Modificação para a tela de log

--- trunk/OnLance/src/Utils/UtilsConstants.java
+++ trunk/OnLance/src/Utils/UtilsConstants.java
@@ -8,6 +8,17 @@ public class UtilsConstants {
 	public final static String QUANT_GOLS = "quantidadeGols";
 	public final static String QUANT_JOGADORES = "quantidadeJogadores";
 	public final static String TEMPO_PARTIDA = "tempoPartida";
+	public final static String TEMPO = "tempo";
+	public final static String DESCRICAO = "descricao";
+	public final static String JOGADOR = "jogador";
+	public final static String EVENTO = "evento";
+	public final static String GOL = "gol";
+	public final static String INICIO = "Inicio";
+	public final static String TERMINO = "Termino";
+	public final static String TIME1= "TIME1";
+	public final static String TIME2= "TIME2";
+	public final static String PLACAR_TIME1= "PLACARTIME1";
+	public final static String PLACAR_TIME2= "PLACARTIME2";
 	
 	public final  static List<String> permissions_app
 	= Arrays.asList("email","public_profile","user_friends","publish_actions");
