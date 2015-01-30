ea0671fa86b0e8fdfa4efa9420be438ef7052e65
kcpo <kcpo@cin.ufpe.br>
2015-01-30 03:55:12 +0000

    [kcpo] fazendo a tela de cadastrar jogador no grupo

--- OnLance/src/com/app/onlance/GrupoPerfilActivity.java
+++ OnLance/src/com/app/onlance/GrupoPerfilActivity.java
@@ -1,12 +1,71 @@
 package com.app.onlance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.app.adapter.BaseAdapterAmigos;
+import com.app.facade.JogadorFacade;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class GrupoPerfilActivity extends Activity {
+
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.grupo_perfil);
+		init();
+	}
+	
+	
+	private void init() {
+		ListView list = (ListView) findViewById(R.id.listPerfilGrupo);
+
+		List<JogadorFacade> amigos = new ArrayList<JogadorFacade>();
+		JogadorFacade kaio = new JogadorFacade();
+		JogadorFacade geyson = new JogadorFacade();
+		JogadorFacade helton = new JogadorFacade();
+		JogadorFacade joao = new JogadorFacade();
+		JogadorFacade ze = new JogadorFacade();
+
+		kaio.setNome("Kaio");
+		kaio.getHistorico().setGol(10);
+		kaio.getHistorico().setJogos(10);
+
+		geyson.setNome("Geyson");
+		geyson.getHistorico().setGol(12);
+		geyson.getHistorico().setJogos(20);
+
+		helton.setNome("helton");
+		helton.getHistorico().setGol(1);
+		helton.getHistorico().setJogos(50);
+
+		joao.setNome("joao");
+		joao.getHistorico().setGol(1);
+		joao.getHistorico().setJogos(10);
+
+		ze.setNome("ze");
+		ze.getHistorico().setGol(1000);
+		ze.getHistorico().setJogos(1);
+
+		amigos.add(kaio);
+		amigos.add(geyson);
+		amigos.add(helton);
+		amigos.add(joao);
+		amigos.add(ze);
+
+		BaseAdapterAmigos adpter = new BaseAdapterAmigos(this, amigos);
+
+		list.setAdapter(adpter);
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,10 +87,5 @@ public class GrupoPerfilActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.grupo_perfil);
-	}
 
 }
